bdd1e4d01daf874997c4857e1fe5e6ad39947e53
etoile88 <ryotnns99@gmail.com>
2024-06-22 21:39:00 +0900

    READD　初期設定＆基礎実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
-
-
 class PostController extends Controller
 {
-    public function index(Post, $post) 
+    public function index(Post $post) 
     {
         return $post->get();
     }
