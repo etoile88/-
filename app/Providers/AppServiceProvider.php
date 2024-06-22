0d8bb0245a1dddb7cbbaf66402e13812d32842f7
etoile88 <ryotnns99@gmail.com>
2024-06-22 23:18:28 +0900

    8-2　課題完

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function boot(): void
     {
-        //
+        Paginator::useBootstrap();
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
 }
